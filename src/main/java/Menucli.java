416dfbd97e6673e1841a7b0ce7983e39f9296e47
ThomasDBM <thomas.debeaumont@free.fr>
2022-02-10 21:53:56 +0000

    Test run pplication on gitpod

--- src/main/java/Menucli.java
+++ src/main/java/Menucli.java
@@ -1,15 +1,15 @@
-import picocli.CommandLine;
-import picocli.CommandLine.Command;
-import picocli.CommandLine.Option;
-import picocli.CommandLine.Parameters;
-
 import java.io.File;
 import java.math.BigInteger;
 import java.nio.file.Files;
 import java.security.MessageDigest;
 import java.util.concurrent.Callable;
 
-@Command(name = "checksum", mixinStandardHelpOptions = true, version = "checksum 4.0",
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+@Command(name = "Menucli", mixinStandardHelpOptions = true, version = "checksum 4.0",
         description = "Prints the checksum (SHA-256 by default) of a file to STDOUT.")
 class Menucli implements Callable<Integer> {
 
